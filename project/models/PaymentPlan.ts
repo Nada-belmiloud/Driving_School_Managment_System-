4681090a728cedb1fa3e81c4074953798636d5ec
Nada-belmiloud <152468122+Nada-belmiloud@users.noreply.github.com>
2025-11-28 17:32:16 +0100

    Update PaymentPlan.ts

--- project/models/PaymentPlan.ts
+++ project/models/PaymentPlan.ts
@@ -1,20 +1,16 @@
 // project/models/PaymentPlan.ts
-import mongoose, { Schema, Document, model, models } from "mongoose";
+import mongoose, { Schema, model, models } from "mongoose";
 
-export interface IPaymentPlan extends Document {
+export interface IPaymentPlan {
   name: string;
   numberOfInstallments: number;
   totalAmount: number;
 }
 
-const PaymentPlanSchema = new Schema<IPaymentPlan>(
-  {
-    name: { type: String, required: true },
-    numberOfInstallments: { type: Number, required: true },
-    totalAmount: { type: Number, required: true },
-  },
-  { timestamps: true }
-);
+const PaymentPlanSchema = new Schema<IPaymentPlan>({
+  name: { type: String, required: true },
+  numberOfInstallments: { type: Number, required: true },
+  totalAmount: { type: Number, required: true },
+}, { timestamps: true });
 
-export default (models.PaymentPlan as mongoose.Model<IPaymentPlan>) ||
-  model<IPaymentPlan>("PaymentPlan", PaymentPlanSchema);
+export default (models.PaymentPlan as mongoose.Model<IPaymentPlan>) || model<IPaymentPlan>("PaymentPlan", PaymentPlanSchema);
