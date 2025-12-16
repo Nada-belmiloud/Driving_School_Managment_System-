2ed61c5f54c3c10199825cec84220e96e338ea2e
Nada <nada.belmiloud@ensia.edu.dz>
2025-12-16 20:00:42 +0100

    transform from web to desktop

---
+++ project/main.js
@@ -0,0 +1,29 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  // Load Next.js frontend
+  win.loadURL('http://localhost:3000');
+
+  // Optional: open DevTools
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
